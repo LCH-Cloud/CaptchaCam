45cf09ca11e4e6b4aa4bf554a6bf1b001e3fe640
Dominik Spitzli <dominik@spitzli.dev>
2023-07-20 11:06:12 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,185 @@
+use chrono::prelude::*;
+use clap::Parser;
+use nokhwa::query;
+use nokhwa::utils::ApiBackend;
+use nokhwa::{
+    pixel_format::RgbFormat,
+    utils::{CameraIndex, RequestedFormat, RequestedFormatType},
+    Camera,
+};
+use std::thread;
+use std::time::Instant;
+use tracing::{debug, error, info, warn};
+
+/// Provides a command line interface for controlling the camera.
+#[derive(Parser, Debug)]
+#[command(
+    author,
+    version,
+    about = "A command line interface for controlling the camera."
+)]
+struct Args {
+    /// Sets the delay (in seconds) before the camera starts taking pictures.
+    #[structopt(long, default_value = "0")]
+    delay: u64,
+
+    /// Sets the filename (as a string) of the image.
+    #[structopt(long, default_value = "image")]
+    filename: String,
+
+    /// Specifies the camera name.
+    #[structopt(long)]
+    devname: Option<String>,
+
+    /// Sets the camera number.
+    #[structopt(long, default_value = "0")]
+    devnum: u32,
+
+    /// Lists all available cameras. [default: false]
+    #[structopt(long, default_value = "false")]
+    devlist: bool,
+
+    /// Enables or disables the output. [default: false]
+    #[structopt(long, default_value = "false")]
+    quiet: bool,
+
+    /// Enables debug mode. [default: false]
+    #[structopt(long, default_value = "false")]
+    debug: bool,
+}
+
+fn main() {
+    // We start by capturing the start time for overall performance monitoring.
+    let total_start = Instant::now();
+    // Parse the command line arguments into an Args struct.
+    let args = Args::parse();
+
+    let delay = args.delay;
+    let filename = args.filename;
+    let devname = args.devname;
+    let devnum = args.devnum;
+    let devlist = args.devlist;
+    let quiet = args.quiet;
+    let debug = args.debug;
+    let mut index = CameraIndex::Index(devnum);
+
+    // Initialize the tracing subscriber for logging, unless it's turned off by 'quiet' or 'debug' flag.
+    if !quiet && !debug {
+        tracing_subscriber::fmt::fmt().with_ansi(false).init();
+    }
+
+    if debug {
+        tracing_subscriber::fmt::fmt()
+            .with_max_level(tracing::Level::DEBUG)
+            .with_ansi(false)
+            .init()
+    }
+
+    // Query available cameras and handle any error.
+    let cameras = match query(ApiBackend::Auto) {
+        Ok(cameras) => cameras,
+        Err(e) => {
+            error!("Failed to query cameras: {:?}", e);
+            return;
+        }
+    };
+
+    // If devlist is enabled, list all available cameras.
+
+    if devlist {
+        for camera in cameras {
+            println!(
+                "devname: {} | devnum: {}",
+                camera.human_name(),
+                camera.index()
+            );
+        }
+        return;
+    }
+
+    // If devname is provided, search for the camera in the list of available cameras.
+    if let Some(devname) = devname {
+        for camera in cameras {
+            if camera.human_name() == devname {
+                index = camera.index().clone();
+            }
+        }
+    }
+
+    // Sleep for the specified delay time.
+    thread::sleep(std::time::Duration::new(delay, 0));
+
+    // Create a request for the highest possible resolution CameraFormat that can be decoded to RGB.
+    let requested =
+        RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestFrameRate);
+
+    // Create a new camera instance and handle any error.
+    let mut camera = match Camera::new(index, requested) {
+        Ok(camera) => camera,
+        Err(e) => {
+            error!("Failed to create a new camera instance: {:?}", e);
+            return;
+        }
+    };
+
+    let capture_start = Instant::now();
+
+    // Capture a frame from the camera and handle any error.
+    let frame = match camera.frame() {
+        Ok(frame) => frame,
+        Err(e) => {
+            error!("Failed to capture frame: {:?}", e);
+            return;
+        }
+    };
+    let capture_end = Instant::now();
+    let capture_duration = capture_end.duration_since(capture_start);
+
+    debug!(
+        "Captured Single Frame of {}, Capture Time: {:?}",
+        frame.buffer().len(),
+        capture_duration
+    );
+
+    let decode_start = Instant::now();
+
+    // Decode the captured frame into an ImageBuffer and handle any error.
+    let decoded = match frame.decode_image::<RgbFormat>() {
+        Ok(decoded) => decoded,
+        Err(e) => {
+            error!("Failed to decode image: {:?}", e);
+            return;
+        }
+    };
+    let decode_end = Instant::now();
+    let decode_duration = decode_end.duration_since(decode_start);
+
+    debug!(
+        "Decoded Frame of {}, Decode Time: {:?}",
+        decoded.len(),
+        decode_duration
+    );
+
+    let save_start = Instant::now();
+
+    // Save the decoded image to a file and handle any error.
+    if let Err(e) = decoded.save(format!("{}.png", filename)) {
+        error!("Failed to save image: {:?}", e);
+    };
+
+    let save_end = Instant::now();
+    let save_duration = save_end.duration_since(save_start);
+
+    debug!("Image Saved, Save Time: {:?}", save_duration);
+
+    let total_end = Instant::now();
+    let total_duration = total_end.duration_since(total_start);
+
+    // Print out the total time for performance monitoring.
+    debug!("Total Time: {:?}", total_duration);
+
+    // Print the camera info, image resolution and filename.
+    info!("Camera: {}", camera.info().human_name());
+    info!("Resolution: {}x{}", decoded.width(), decoded.height());
+    info!("Image: {}", format!("{}.png", filename));
+}
